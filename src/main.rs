8f21b16babedfbf47b122b00bc60590cf1c10057
Mike Austin <mike_ekim@yahoo.com>
2025-05-05 12:23:13 -0700

    Convert use of references; Create table constructor

--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,93 @@
 #![allow(dead_code)]
 #![allow(unused_variables)]
 
-#[derive(Debug)]
+mod test;
 
+#[derive(Debug)]
 enum FieldType {
     Int32,
     Int64,
     String,
+    // VarString(i16),
 }
 
 #[derive(Debug)]
-struct Field<'a> {
-    name: &'a str,
+struct Field {
+    name: String,
     kind: FieldType,
 }
 
 #[derive(Debug)]
-struct Schema<'a> {
-    fields: &'a [Field<'a>],
+struct Schema {
+    fields: Vec<Field>,
 }
 
 #[derive(Debug)]
-struct Table<'a> {
-    name: &'a str,
-    schema: Schema<'a>,
-    data: &'a mut Vec<u8>,
+struct Table {
+    name: String,
+    schema: Schema,
+    data: Vec<u8>,
+}
+
+impl Table {
+    pub fn new(name: &str, schema: Schema) -> Table {
+        Self {
+            name: name.to_string(),
+            schema,
+            data: vec![0u8; 16],
+        }
+    }
+
+    pub fn set_field(&mut self, field_index: usize, value: i32) {
+        let ptr: *mut u8 = self.data.as_mut_ptr();
+        let mut offset: usize = 0;
+
+        for i in 0..field_index {
+            match self.schema.fields[i].kind {
+                FieldType::Int32 => offset += 4,
+                FieldType::Int64 => offset += 4,
+                FieldType::String => offset += 4,
+            }
+        }
+    }
 }
 
 fn main() {
-    let schema: Schema<'_> = Schema {
-        fields: &[
+    let schema = Schema {
+        fields: vec![
             Field {
-                name: "id",
+                name: "id".to_string(),
                 kind: FieldType::Int32,
             },
             Field {
-                name: "title",
+                name: "title".to_string(),
                 kind: FieldType::String,
             },
             Field {
-                name: "estimate",
+                name: "estimate".to_string(),
                 kind: FieldType::Int64,
             },
         ],
     };
 
-    let table = Table {
-        name: "items",
-        schema: schema,
-        data: &mut vec![0u8; 16],
-    };
+    let mut table = Table::new("items", schema);
 
     let ptr: *mut u8 = table.data.as_mut_ptr();
     let mut offset: usize = 0;
+
+    /*
+        Indexing into field
+        Very large string would require 2 byte indexes
+        Separate file for variable size strings?
+
+        Field 0 => 0
+        Field 1 => 4
+        Field 2 => 8
+
+        11 = 4
+        1111 = 16
+        11111111 = 256
+    */
 
     for (i, field) in table.schema.fields.iter().enumerate() {
         unsafe {
