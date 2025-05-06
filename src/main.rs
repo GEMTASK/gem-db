acf06f408d9ea058f8333fa6a39c6672b19da9cb
Mike Austin <mike_ekim@yahoo.com>
2025-05-06 13:28:11 -0700

    Add comments_table

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ mod table;
 use table::{Column, Table, Type, Value};
 
 fn main() {
-    let mut table = Table::new(
+    let mut items_table = Table::new(
         "items",
         vec![
             Column::new("id", Type::Int32),
@@ -14,21 +14,30 @@ fn main() {
         ],
     );
 
-    table.select();
+    let mut comments_table = Table::new(
+        "comments",
+        vec![
+            Column::new("id", Type::Int32),
+            // Column::new("item_id", Type::Relation { table: items_table }),
+            Column::new("comment", Type::Int32),
+        ],
+    );
 
-    table.insert(&[
+    items_table.select();
+
+    items_table.insert(&[
         Value::Int32(255),
         Value::String("ÀÀ".to_string()),
         Value::Int64(65535),
     ]);
 
-    table.insert(&[
+    items_table.insert(&[
         Value::Int32(10),
         Value::String("AA".to_string()),
         Value::Int64(20),
     ]);
 
-    table.select();
+    items_table.select();
 
-    println!("{:#?}", table);
+    println!("{:#?}", items_table);
 }
