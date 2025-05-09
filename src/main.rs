d544566477077acf8d0cd2b411def22c02942637
Mike Austin <mike_ekim@yahoo.com>
2025-05-08 18:40:02 -0700

    Add example test

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,21 @@ use table::{Column, Query, Relation, RelationType, Table, Type, Value};
 
 const QUERY: Query = Query::Eq("id", 255);
 
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
+
 fn main() {
     let items_table = Table::new(
         "items",
